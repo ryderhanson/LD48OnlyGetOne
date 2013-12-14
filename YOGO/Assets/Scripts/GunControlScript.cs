303418a24e5289d3324911de312769705ba8cda8
ryderhanson <ryder.hanson@gmail.com>
2013-12-14 13:22:33 -0800

    Scene setup and gun mechanic mostly working'

---
+++ YOGO/Assets/Scripts/GunControlScript.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunControlScript : MonoBehaviour 
+{
+	public float rotateSpeed = 1.0f;
+	public GameObject guide;
+
+	// Use this for initialization
+	void Start () 
+	{
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		/*Vector3 eulerRotation = new Vector3();
+
+		//Rotating at the same time is bad
+		if(Input.GetKey(KeyCode.A))
+		{
+			eulerRotation.y += rotateSpeed;
+		}
+		else if(Input.GetKey(KeyCode.D))
+		{
+			eulerRotation.y -= rotateSpeed;
+		}
+		else if(Input.GetKey(KeyCode.W))
+		{
+			eulerRotation.x += rotateSpeed;
+		}
+		else if(Input.GetKey(KeyCode.S))
+		{
+			eulerRotation.x -= rotateSpeed;
+		}
+		
+		
+		transform.Rotate(eulerRotation);*/
+
+		transform.LookAt(guide.transform.position);
+	}
+
+	void Flip()
+	{
+
+	}
+}
---
+++ YOGO/Assets/Scripts/GunGuide.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunGuide : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		Vector3 newPos = Input.mousePosition;
+		//newPos.z = transform.position.z - Camera.main.transform.position.z;
+		transform.position = Camera.main.ScreenToWorldPoint(new Vector3(newPos.x, newPos.y, 3.0f));
+	}
+}
